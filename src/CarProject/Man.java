257b54bc380816fed456aacbd1d66d78e15b5f0e
OLI2018 <37943857+OLI2018@users.noreply.github.com>
2020-10-15 13:47:33 -0700

    Java Rush - StringComparison

---
+++ src/CarProject/Man.java
@@ -0,0 +1,25 @@
+package CarProject;
+
+public class Man {
+
+  int dnaCode;
+
+  public boolean equals(Man x) {
+   return this.dnaCode ==  x.dnaCode;
+
+  }
+
+  public static void main(String[] args) {
+
+    Man man1 = new Man();
+    man1.dnaCode = 1;
+
+    Man man2 = new Man();
+    man2.dnaCode = 1;
+
+    System.out.println(man1.equals(man2));
+
+  }
+}
+
+
---
+++ src/CarProject/StringComparison.java
@@ -0,0 +1,13 @@
+package CarProject;
+
+public class StringComparison {
+
+  public static void main(String[] args) {
+
+    String s1 = "Joe";
+
+    String s2 = new String("JOE");
+
+    System.out.println(s1.equalsIgnoreCase(s2));
+  }
+}
