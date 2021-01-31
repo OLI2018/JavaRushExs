908c7988022971a2b72b5c0208acec59b42d788d
OLI2018 <37943857+OLI2018@users.noreply.github.com>
2021-01-31 10:53:49 -0800

    Java Rush - Multithreading Interrupts -- task in Java

---
+++ src/InterruptTasks/Interrupt01.java
@@ -0,0 +1,44 @@
+// Java Core,  6 уровень,  10 лекция
+// Считаем секунды
+
+package InterruptTasks;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Interrupt01 {
+
+  public static void main(String[] args) throws IOException {
+
+    InputStreamReader in = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(in);
+    //create and run thread
+    Stopwatch stopwatch = new Stopwatch();
+    stopwatch.start();
+    //read a string
+    reader.readLine();
+    stopwatch.interrupt();
+    //close streams
+    reader.close();
+    in.close();
+  }
+
+  public static class Stopwatch extends Thread {
+    private int seconds;
+
+    public void run() {
+      try {
+
+        while (!isInterrupted()) {
+          Thread.sleep(1000);
+          seconds++;
+        }
+      } catch (InterruptedException e) {
+        System.out.println(seconds);
+      }
+    }
+  }
+}
+
+
---
+++ src/InterruptTasks/Interrupt04.java
@@ -0,0 +1,64 @@
+// Java Core,  6 уровень,  10 лекция
+// Один для всех, все - для одного
+
+package InterruptTasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Interrupt04 {
+
+  public static byte threadCount = 3;
+
+  static List<Thread> threads = new ArrayList<Thread>(threadCount);
+
+  public static void main(String[] args) throws InterruptedException {
+    initThreadsAndStart();
+    Thread.sleep(3000);
+    ourInterruptMethod();
+  }
+
+  public static void ourInterruptMethod() {
+
+    for (int i = 0; i < threadCount; i++) {
+      threads.get(i).interrupt();
+
+    }
+  }
+
+  private static void initThreadsAndStart() {
+    Water water = new Water("water");
+    for (int i = 0; i < threadCount; i++) {
+      threads.add(new Thread(water, "#" + i));
+    }
+
+    for (int i = 0; i < threadCount; i++) {
+      threads.get(i).start();
+    }
+  }
+
+  public static class Water implements Runnable {
+    private String sharedResource;
+
+    public Water(String sharedResource) {
+      this.sharedResource = sharedResource;
+    }
+
+    public void run() {
+
+      boolean isCurrentThreadInterrupted = Thread.currentThread().isInterrupted();
+      String threadName = Thread.currentThread().getName();
+
+      try {
+        while (!isCurrentThreadInterrupted) {
+
+          System.out.println("Объект " + sharedResource + ", нить " + threadName);
+          Thread.sleep(1000);
+        }
+      } catch (InterruptedException e) {
+      }
+    }
+  }
+}
